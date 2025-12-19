b19ddd3559fc2a39e3105f1a26eeeb79eb42b6b5
Dima <Sda201018@gmail.com>
2025-12-19 22:54:57 +0300

    add function IsAdmin to sqlite

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -70,3 +70,21 @@ func (s *Storage) User(ctx context.Context, email string) (models.User, error) {
 	}
 	return user, nil
 }
+
+func (s *Storage) IsAdmin(ctx context.Context, userID int64) (bool, error) {
+	const op = "storage.sqlite.IsAdmin"
+
+	stmt, err := s.db.Prepare("SELECT admin FROM users WHERE id = ?")
+	if err != nil {
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+	row := stmt.QueryRowContext(ctx, userID)
+	var isAdmin bool
+	if err := row.Scan(&isAdmin); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return false, fmt.Errorf("%s: %w", op, storage.ErrAppNotFound)
+		}
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+	return isAdmin, nil
+}
