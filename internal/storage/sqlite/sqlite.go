ec1f9bbf0f0c477be4041c12487efa04f0476143
Dima <Sda201018@gmail.com>
2025-12-19 22:25:11 +0300

    add SaveUser function

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -1,9 +1,13 @@
 package sqlite
 
 import (
+	"authService/internal/storage"
+	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 
+	"github.com/mattn/go-sqlite3"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -22,4 +26,26 @@ func New(storagePath string) (*Storage, error) {
 	return &Storage{
 		db: db,
 	}, nil
+}
+
+func (s *Storage) SaveUser(ctx context.Context, email string, Passhash []byte) (int64, error) {
+	const op = "storage.sqlite.SaveUser"
+
+	stmt, err := s.db.Prepare("INSERT INTO users (email, pass_hash) VALUES (?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	res, err := stmt.ExecContext(ctx, email, Passhash)
+	if err != nil {
+		var sqliteErr sqlite3.Error
+		if errors.As(err, &sqliteErr) && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrUserExists)
+		}
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	return id, nil
 }
