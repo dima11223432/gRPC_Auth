2f3955cb4d31a1c27ad34e72a194af96f5048d8c
Dima <Sda201018@gmail.com>
2025-12-19 21:05:59 +0300

    released migrator successfully!

--- cmd/migrator/main.go
+++ cmd/migrator/main.go
@@ -1,6 +1,12 @@
 package migrator
 
-import "flag"
+import (
+	"errors"
+	"flag"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+)
 
 func main() {
 	var storagePath, migrationPath, migrationTable string
@@ -8,4 +14,27 @@ func main() {
 	flag.StringVar(&migrationPath, "migration", "", "path to migration")
 	flag.StringVar(&migrationTable, "table", "", "migration table")
 	flag.Parse()
+
+	if storagePath == "" {
+		panic("storage path is required")
+	}
+	if migrationPath == "" {
+		panic("migration path is required")
+	}
+	m, err := migrate.New(
+		"file://"+migrationPath,
+		fmt.Sprintf("sqlite3://%s?x-migration-table=%s", storagePath, migrationTable),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("no migration to apply")
+			return
+		}
+		panic(err)
+	}
+	fmt.Println("migrations applied successfully")
 }
